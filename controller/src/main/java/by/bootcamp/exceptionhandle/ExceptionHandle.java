8a7877c842feb05ea60af4ffa2f3e068fd418580
NAUM <naum.st.123@gmail.com>
2023-03-26 20:22:50 +0300

    con: end work with exception handler

--- controller/src/main/java/by/bootcamp/exceptionhandle/ExceptionHandle.java
+++ controller/src/main/java/by/bootcamp/exceptionhandle/ExceptionHandle.java
@@ -1,12 +1,18 @@
 package by.bootcamp.exceptionhandle;
 
+import by.bootcamp.exception.EntityAlreadyExistException;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
+import org.springframework.validation.ObjectError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import java.time.LocalDateTime;
 import java.util.Collections;
+import java.util.Map;
 import java.util.UUID;
 
 @RestControllerAdvice
@@ -14,11 +20,38 @@ public class ExceptionHandle {
 
     private static final String ERROR = "error";
 
-    @ExceptionHandler(Exception.class)
-    public ResponseEntity<Object> handleNoSuchEntityException(Exception e) {
+    @ExceptionHandler({IllegalArgumentException.class, HttpMessageNotReadableException.class,
+            EntityAlreadyExistException.class, DataIntegrityViolationException.class})
+    public ResponseEntity<Map<String, ErrorContainer>> handleIllegalArgumentException(Exception e) {
 
         return new ResponseEntity<>(
-                Collections.singletonMap(ERROR, composeErrorContainer(e, 2)),
+                Collections.singletonMap(ERROR, composeErrorContainer(e, 400)),
+                HttpStatus.BAD_REQUEST
+        );
+    }
+
+    @ExceptionHandler({MethodArgumentNotValidException.class})
+    public ResponseEntity<Map<String, ErrorContainer>> handleMethodArgumentNotValidException(MethodArgumentNotValidException e) {
+
+        ErrorContainer errorContainer = composeErrorContainer(e, 400);
+
+        StringBuilder sb = new StringBuilder();
+        for (ObjectError error : e.getBindingResult().getAllErrors()) {
+            sb.append(error.getDefaultMessage()).append('\n');
+        }
+        errorContainer.setMessage(sb.substring(0, sb.length() - 1));
+
+        return new ResponseEntity<>(
+                Collections.singletonMap(ERROR, errorContainer),
+                HttpStatus.BAD_REQUEST
+        );
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<Map<String, ErrorContainer>> handleNoSuchEntityException(Exception e) {
+
+        return new ResponseEntity<>(
+                Collections.singletonMap(ERROR, composeErrorContainer(e, 500)),
                 HttpStatus.NOT_FOUND
         );
     }
